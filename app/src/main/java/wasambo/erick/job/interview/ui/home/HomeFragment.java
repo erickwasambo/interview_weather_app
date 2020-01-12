2693ab1ac92a0db433628db6cf063107c0ea6f62
Erick Wasambo <erick.wasambo@apollo.co.ke>
2020-01-12 11:10:06 +0300

    added data binding

--- app/src/main/java/wasambo/erick/job/interview/ui/home/HomeFragment.java
+++ app/src/main/java/wasambo/erick/job/interview/ui/home/HomeFragment.java
@@ -8,28 +8,35 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import wasambo.erick.job.interview.R;
+import wasambo.erick.job.interview.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        this.binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_home, container, false);
+        View rootView = binding.getRoot();
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        //final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return rootView;
     }
 }
